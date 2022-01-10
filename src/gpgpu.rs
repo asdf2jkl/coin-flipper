d3f7571a06b66bffc88a5e2371bbfe9f2fc82181
Saladin <23lafrenip@student.hpts.us>
2022-01-09 23:01:13 -0500

    fixed gpu results when count / threads > u32::MAX

--- src/gpgpu.rs
+++ src/gpgpu.rs
@@ -1,3 +1,17 @@
+/*
+    TODO:
+    - Add support for rng buffer reuse on inputs with count / device_threads > 2e32
+    - Optimize final reduction (currently it's done on gpu, but it could be done on cpu (idk if the reduction is faster on cpu than on gpu, will have to benchmark))
+    - Runtime selection of vectored vs scalar kernel (based on device properties)
+    - Async execution of remainder on cpu
+    - Add support for multiple devices (meh, not sure if it's worth bothering)
+    - Optimze for seed generation (could use simd intrinsics, all it takes is a new chacha implementation)
+    - Cleanup, document, and validate both host and device code
+    - Properly handle errors (currently it's just a panic)
+    - Probably a lot more
+*/
+
+
 use std::{ffi::c_void, ptr, simd::u32x4};
 
 use opencl3::{
@@ -23,7 +37,7 @@ union xoshiro128plusplus {
 }
 
 pub fn gpu_executor(count: u64) -> Option<u64> {
-    if count < 1_000_000_000 {
+    if count < 100_000_000_000 {
         return threaded_wrapper(count);
     }
 
@@ -43,8 +57,17 @@ pub fn gpu_executor(count: u64) -> Option<u64> {
         return None;
     };
 
-    let threads =
-        Device::max_compute_units(&Device::new(context.default_device())).unwrap() * 64 * 8;
+    let threads = {
+        let device_threads = Device::max_compute_units(&Device::new(context.default_device()))
+            .unwrap() as u64
+            * 64
+            * 8;
+        if count / device_threads > u32::MAX as u64 {
+            device_threads * ((count / device_threads) / u32::MAX as u64 + 1)
+        } else {
+            device_threads
+        }
+    };
 
     let program = Program::create_and_build_from_source(&context, KERNEL_SOURCE, "").unwrap();
 
@@ -75,52 +98,24 @@ pub fn gpu_executor(count: u64) -> Option<u64> {
     kernel.set_arg_local_buffer(3, 8).unwrap();
 
     let queue = CommandQueue::create(&context, context.default_device(), 0).unwrap();
-    let mut result = 0;
 
-    for _ in 0..((count / threads as u64) / u32::MAX as u64) {
-        kernel.set_arg::<u32>(0, &(u32::MAX)).unwrap();
+    kernel
+        .set_arg::<u32>(0, &((count / threads).try_into().unwrap()))
+        .unwrap();
+    queue
+        .enqueue_nd_range_kernel(
+            kernel.get(),
+            1,
+            ptr::null(),
+            [threads as usize].as_ptr(),
+            [64 as usize].as_ptr(),
+            &[],
+        )
+        .unwrap();
+    let mut result_buffer: [u64; 1] = [0];
+    queue
+        .enqueue_read_buffer(&output, true as u32, 0, &mut result_buffer, &[])
+        .unwrap();
 
-        queue
-            .enqueue_nd_range_kernel(
-                kernel.get(),
-                1,
-                ptr::null(),
-                [threads as usize].as_ptr(),
-                [64 as usize].as_ptr(),
-                &[],
-            )
-            .unwrap();
-        let mut result_buffer: [u64; 1] = [0];
-        queue
-            .enqueue_read_buffer(&output, true as u32, 0, &mut result_buffer, &[])
-            .unwrap();
-        result += result_buffer[0];
-    }
-    {
-        kernel
-            .set_arg::<u32>(
-                0,
-                &(((count / threads as u64) % u32::MAX as u64)
-                    .try_into()
-                    .unwrap()),
-            )
-            .unwrap();
-        queue
-            .enqueue_nd_range_kernel(
-                kernel.get(),
-                1,
-                ptr::null(),
-                [threads as usize].as_ptr(),
-                [64 as usize].as_ptr(),
-                &[],
-            )
-            .unwrap();
-        let mut result_buffer: [u64; 1] = [0];
-        queue
-            .enqueue_read_buffer(&output, true as u32, 0, &mut result_buffer, &[])
-            .unwrap();
-        result += result_buffer[0];
-    }
-
-    Some(result + threaded_wrapper(count % threads as u64).unwrap())
+    Some(result_buffer[0] + threaded_wrapper(count % threads as u64).unwrap())
 }
